34ed43bc70ce14645ada998f90b54dcc2471e618
chang shunshun <yghysdr@163.com>
2017-06-01 10:31:37 +0800

    update code

--- api.js
+++ api.js
@@ -1,14 +1,14 @@
 /**
  * Created by yghysdr on 2017/5/27.
  */
-const utils = require('./utils');
+// const utils = require('./utils');
 
-let base = "http://" + utils.getIp()[1] + ":3000/";
-let baseUrl = base + '/api/';
+// let base = "http://" + utils.getIp()[1] + ":3000/";
+// let baseUrl = base + '/api/';
 let baseFileUrl = "http://oqlwlmr6l.bkt.clouddn.com/";
 
 module.exports = {
-    baseUrl: baseUrl,
-    base: base,
+    // baseUrl: baseUrl,
+    // base: base,
     baseFileUrl: baseFileUrl
 };
