fb03b5db7ee43d456d5745a0e13a94b27d2fb04a
chang shunshun <yghysdr@163.com>
2017-06-19 11:28:39 +0800

    favicon.ico

--- controllers/user.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var fn_hello = async (ctx, next) => {
-    var name = ctx.params.name;
-    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
-};
-
-module.exports = {
-    'GET /hello/:name': fn_hello
-};
